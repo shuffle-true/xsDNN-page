4d190d9b23bc3a4fba77a12986b28a7ba7ff99b0
kraken-kryak <rozhinthe@ya.ru>
2022-07-23 21:10:55 +0300

    небольшие изменения

--- class_fully_connected.js
+++ class_fully_connected.js
@@ -5,6 +5,7 @@ var class_fully_connected =
     [ "backprop", "class_fully_connected.html#abc92fc51eb2dd0d64c5fb5bf2021e4c1", null ],
     [ "backprop_data", "class_fully_connected.html#a874de85bb5b8377ffe24590a25f9f27c", null ],
     [ "distribution_type", "class_fully_connected.html#a9d04183e6ab8f561858166016bd051f7", null ],
+    [ "eval", "class_fully_connected.html#af409b28e4672bef875f79a3163bc5caa", null ],
     [ "fill_meta_info", "class_fully_connected.html#af87897142a76a9d9bc1cfc4c92633420", null ],
     [ "forward", "class_fully_connected.html#a44dd0014fb832068c62567c71f9a588d", null ],
     [ "get_derivatives", "class_fully_connected.html#a066e67d103bd6abbc947654bc55a62ff", null ],
@@ -14,5 +15,6 @@ var class_fully_connected =
     [ "layer_type", "class_fully_connected.html#a21a2fd0e1bfc0e45b35dfe40c4671417", null ],
     [ "output", "class_fully_connected.html#a2c1b77310bd00ea5f107fe6bedd648d0", null ],
     [ "set_parametrs", "class_fully_connected.html#a21dfd80fcb5b2c054f5c4c34598fb679", null ],
+    [ "train", "class_fully_connected.html#ad6163e94dccc3fd35b1afb7a92c4925f", null ],
     [ "update", "class_fully_connected.html#a5edfef61d185a3a7a2297b87f312fe61", null ]
 ];
